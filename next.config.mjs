90ad86f4f30b0c16b3a8d199e219c3049c53b4cb
Umang Saxena <hanusaxena68@gmail.com>
2025-05-04 19:33:26 +0530

    Image source added in nextjs config file

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'via.placeholder.com',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wallpaperaccess.com',
+        pathname: '/**',
       }
     ],
   },
